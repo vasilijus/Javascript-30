ae74ce5b010e3aec257f2c342f57aad903f86595
vasilijus <vasiliokas@gmail.com>
2019-11-02 18:16:27 +0000

    Learnied transition ends event listener , and also classList toggle

--- Day 5 - Image gallery/index.js
+++ Day 5 - Image gallery/index.js
@@ -5,7 +5,11 @@ function toggleOpen() {
 }
 
 function toggleActive(e) {
-    console.log(e)
+    console.log(e.propertyName)
+    if( e.propertyName.includes('flex') ) {
+        this.classList.toggle('open-active');
+    }
 }
 
 panels.forEach( panel => panel.addEventListener('click',toggleOpen));
+panels.forEach( panel => panel.addEventListener('transitionend', toggleActive));

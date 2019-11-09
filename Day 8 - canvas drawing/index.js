6057134f58511c4afa7214007f90d511b463b947
vasilijus <vasiliokas@gmail.com>
2019-11-09 21:58:37 +0000

    web painting

--- Day 8 - canvas drawing/index.js
+++ Day 8 - canvas drawing/index.js
@@ -1,19 +1,24 @@
 var canvas = document.querySelector('#draw');
 var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
-canvas.height= window.innerHeight;
+canvas.height= (window.innerHeight - 50);
 
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = '20';
+
 
 let isDrawing = false ;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
     if(!isDrawing) return;
-console.log(e)
+// console.log(e)
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
     ctx.lineTo(e.offsetX, e.offsetY);
@@ -21,11 +26,25 @@ console.log(e)
     // lastX = e.pageX ; lastY = e.pageY;
     [lastX, lastY] = [e.offsetX, e.offsetY];
 
+    hue++;
+    if( hue >= 360 ) {
+        hue = 0;
+    }
+
+    if( ctx.lineWidth >= 100 || ctx.lineWidth <= 1 ){
+        direction = !direction;
+    }
+    if(direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
+
 }
 document.addEventListener('mousedown', (e) => {
-    isDrawing = true
+     // lastX = e.offsetX ; lastY = e.offsetY;
     [lastX, lastY] = [e.offsetX, e.offsetY];
-    // lastX = e.offsetX ; lastY = e.offsetY;
+    isDrawing = true;
 });
 document.addEventListener('mousemove', draw);
 

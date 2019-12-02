11993ea2ec0e99eddaafb88306db24ade2241c89
Sergej <sergej@computerassistance.co.uk>
2019-12-02 18:28:26 +0000

    mouse move shadow

--- Day 16 - Mouse event move/index.js
+++ Day 16 - Mouse event move/index.js
@@ -1,7 +1,7 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
 
-const walk = 500; // px
+const walk = 100; // px
 
 function shadow(e) {
     const { offsetWidth: width, offsetHeight: height } = hero;

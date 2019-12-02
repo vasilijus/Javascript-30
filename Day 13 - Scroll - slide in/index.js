63bc352477a1f60614644b6e0a362169bf0161b5
Sergej <sergej@computerassistance.co.uk>
2019-12-02 12:26:58 +0000

    slide in finish

--- Day 13 - Scroll - slide in/index.js
+++ Day 13 - Scroll - slide in/index.js
@@ -2,13 +2,13 @@ function debounce(func, wait = 20, immediate = true) {
     var timeout;
     return function() {
       var context = this, args = arguments;
-      console.log("cont:", context,"args:", args);
+    //   console.log("cont:", context,"args:", args);
       var later = function() {
         timeout = null;
         if (!immediate) func.apply(context, args);
       };
       var callNow = immediate && !timeout;
-      console.log('callNow:',callNow);
+    //   console.log('callNow:',callNow);
       clearTimeout(timeout);
       timeout = setTimeout(later, wait);
       if (callNow) func.apply(context, args);
@@ -19,6 +19,19 @@ function debounce(func, wait = 20, immediate = true) {
 
   function checkSlide(e) {
     //   console.count(e);
+    sliderImages.forEach(slideImage => {
+        // halfway throu the image
+        const slideInAt = (window.scrollY + window.innerHeight) - slideImage.height / 2;
+        // bottom of the image
+        const imageBottom = slideImage.offsetTop;
+        const isHalfShown = slideInAt > slideImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if ( isHalfShown && isNotScrolledPast ) {
+            slideImage.classList.add('active');
+        } else {
+            slideImage.classList.remove('active');
+        }
+    })
 
   }
-  window.addEventListener('scroll', debounce(checkSlide) );
+  window.addEventListener('scroll', debounce(checkSlide, 50) );

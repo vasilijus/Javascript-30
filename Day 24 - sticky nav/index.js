5205cd3136c171761d5d22b6e050c48f9bf32a68
vasilijus <vasiliokas@gmail.com>
2019-12-10 22:54:10 +0000

    sticky nav

--- Day 24 - sticky nav/index.js
+++ Day 24 - sticky nav/index.js
@@ -4,6 +4,11 @@ const topOfNav = nav.offsetTop;
 
 function fixNav() {
     console.log(topOfNav, window.scrollY);
+    if ( window.scrollY >= topOfNav ) {
+        document.body.classList.add('fixed-nav');
+    } else {
+        document.body.classList.remove('fixed-nav');
+    }
 }
 
 document.addEventListener('scroll', fixNav);
